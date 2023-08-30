f9ef408da08007181ba1183ae4cd15d9f844c1bf
Apex-Incarna <emenser05@gmail.com>
2023-08-30 17:06:31 -0400

    Lots of progress, true initial commit

--- script.js
+++
@@ -1,6 +0,0 @@
-// Enlarging Effect as Scrolling
-// document.addEventListener("scroll", function() {
-//   let scrollTop = window.scrollY;
-//   let parallaxImage = document.querySelector(".parallax-wrapper img");
-//   parallaxImage.style.transform = "scale(" + (1 + scrollTop * 0.0005) + ")";
-// });
